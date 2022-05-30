cde96499db47a3e5b66d4ab3bfa6493ab1731e2d
cindyfdes <cindyfernandes0308@gmail.com>
2022-05-30 19:04:16 +0530

    changed purge values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./pages/*.{html,js,tsx}", "./components/*.{html,js,tsx}"],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
